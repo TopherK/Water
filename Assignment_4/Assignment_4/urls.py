e6bddd46b445dbc199295943fd5b60d69543d6e4
TopherK <topherkavallash@gmail.com>
2017-05-03 23:22:08 -0700

    added User filter to Display Reviews page.

--- Assignment_4/Assignment_4/urls.py
+++ Assignment_4/Assignment_4/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
 
     url(r'login/$', views.login, {
         'template_name':'login.html',
-        'authentication_form':LoginForm
+        'authentication_form':LoginForm,
     }, name="login"),
     url(r'logout/$', views.logout,{
         'next_page':'/login'
